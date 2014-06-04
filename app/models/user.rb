afede63d78431b9e2c41fff7d21249e01f9337cb
Daniel Deutsch & msabdeljalil <pair+common-nighthawk.msabdeljalil@devbootcamp.com>
2014-06-04 16:23:39 -0500

    User tests complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :username, presence: true
+  validates :email, presence: true
+
   has_many :authored_arguments, class_name: "Argument", foreign_key: :author_id
   has_many :authored_claims, class_name: "Claim", foreign_key: :author_id
   has_many :votes
