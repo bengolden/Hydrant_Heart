05a81af4b67a18c5114794fea576a302af8a90ed
Anne Baer & Ben Golden <pair+albaer.bengolden@devbootcamp.com>
2014-06-04 14:33:33 -0500

    associations solid

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,6 +1,8 @@
 class Claim < ActiveRecord::Base
-  belongs_to :author, class_name "User"
-  # has_many :arguments_towards, foreign_key: :conclusion_id
-  # has_many :arguments_from
+  belongs_to :author, class_name: "User"
+  has_many :arguments_where_conclusion, class_name: "Argument", foreign_key: :conclusion_id
+  has_many :assumptions_where_premise, class_name: "Assumption", foreign_key: :premise_id
+  has_many :arguments_where_premise, through: :assumptions_where_premise, source: :argument
+  has_many :votes, foreign_key: :voteable_id
 
 end
