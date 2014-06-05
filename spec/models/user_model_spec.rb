3927eeb5d114651f3fe60f96fcfb385577c0145c
Anne Baer & Ben Golden <pair+albaer.bengolden@devbootcamp.com>
2014-06-05 13:54:52 -0500

    adding tests for bcrypt password methods to user model

--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -10,11 +10,23 @@ describe User do
     it "has an attribute email" do
       should allow_value('ddeutsch@book.com').for(:email)
     end
-    it "has an attribute password stuff" do
+    it "has an attribute password" do
       should allow_value('123fsadf45').for(:password)
     end
     it "has an attribute image_url" do
       should allow_value('http://www.image.com/20').for(:image_url)
+    end
+  end
+
+  context "bcrypt password methods" do
+    let!(:rocky) { User.create(username: "rocky", email: "rocky@email.com", password: "password", image_url: "http://dogcarers.com/wp-content/uploads/2014/02/westies-dog-breed2.jpg")}
+    it "alters password before storing in database" do
+      #expect to return a long string not same as entered password
+      expect(rocky.password.to_s).not_to eq("password")
+    end
+
+    it "compares given password to stored password hash properly" do
+      expect(rocky.password_hash.inspect).to eq(rocky.password.inspect)
     end
   end
 
