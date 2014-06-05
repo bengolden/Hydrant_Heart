ba0968f8839dfc22943e11c8213624454ae45a32
Ben Golden <bengolden1@gmail.com>
2014-06-05 11:22:15 -0500

    all tests working for index controller

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,15 +2,67 @@ require 'spec_helper'
 
 describe WelcomeController do
 
-  context "the instance variables are right" do
+  context "the recent claim instance variable is right" do
 
-    it "has the most recent claims in @recent_claims" do
+    it "adds the most recent claim first in @recent_claims" do
+      claim1 = Claim.create(author_id: 1, body: "Hello")
       get :index
-      # claim1 = Claim.create(author_id: 1, body: "Hello")
-      # claimid1 = claim1.id
-      # claim2 = Claim.create(author_id: 1, body: "Goodbye")
-      # claimid2 = claim2.id
-      @recent_claims.first.id.should equal(claimid2)
+      assigns(:recent_claims).first.should eq(claim1)
+    end
+
+    it "adds the second most recent claim second in @recent_claims" do
+      claim1 = Claim.create(author_id: 1, body: "Hello")
+      claim2 = Claim.create(author_id: 1, body: "Bye")
+      get :index
+      assigns(:recent_claims)[1].should eq(claim1)
+    end
+
+    it "only adds 10 items to @recent_claims" do
+      claim1 = Claim.create(author_id: 1, body: "Hello")
+      claim2 = Claim.create(author_id: 1, body: "Hello")
+      claim3 = Claim.create(author_id: 1, body: "Hello")
+      claim4 = Claim.create(author_id: 1, body: "Hello")
+      claim5 = Claim.create(author_id: 1, body: "Hello")
+      claim6 = Claim.create(author_id: 1, body: "Hello")
+      claim7 = Claim.create(author_id: 1, body: "Hello")
+      claim8 = Claim.create(author_id: 1, body: "Hello")
+      claim9 = Claim.create(author_id: 1, body: "Hello")
+      claim10 = Claim.create(author_id: 1, body: "Hello")
+      claim11 = Claim.create(author_id: 1, body: "Hello")
+      get :index
+      assigns(:recent_claims).length.should eq(10)
+    end
+  end
+
+  context "the recent arguments instance variable is right" do
+
+    it "adds the most recent argument first in @recent_arguments" do
+      argument1 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      get :index
+      assigns(:recent_arguments).first.should eq(argument1)
+    end
+
+    it "adds the second most recent argument second in @recent_claims" do
+      argument1 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument2 = Argument.create(author_id: 2, conclusion_id: 2, is_supporting: false)
+      get :index
+      assigns(:recent_arguments)[1].should eq(argument1)
+    end
+
+    it "only adds 10 items to @recent_arguments" do
+      argument1 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument2 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument3 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument4 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument5 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument6 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument7 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument8 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument9 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument10 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      argument11 = Argument.create(author_id: 1, conclusion_id: 1, is_supporting: true)
+      get :index
+      assigns(:recent_arguments).length.should eq(10)
     end
   end
 
