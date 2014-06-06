95def944eadcc651de5b7134a14c3bf7cde27727
Anne Baer & Ben Golden <pair+albaer.bengolden@devbootcamp.com>
2014-06-05 21:08:56 -0500

    create new user page and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
   def index
     user = User.find(session[:user_id])
   end
+
+  #not tested
+  def new
+    @user = User.new #not tested
+  end
+  #not tested
+  def create
+    @user = User.new(user_params) #not tested
+    @user.save! #not tested
+    redirect_to "/users/#{@user.id}"
+  end
+
+  private
+  #do we test private methods?
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :image_url)
+  end
+
+
 end
