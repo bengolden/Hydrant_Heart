15e86e0dc32d017256cea9e95236507d27a190f3
Jared Daugherty <jaredmdaugherty@gmail.com>
2014-06-06 11:13:08 -0500

    Argument tests 11/12

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -1,2 +1,12 @@
 class Argument < ActiveRecord::Base
+	validates :conclusion, presence: true
+	validates :author, presence: true
+
+	has_many :assumptions
+  	has_many :premises, through: :assumptions
+	belongs_to :author, class_name: "User"
+	belongs_to :conclusion, class_name: "Claim"
+	has_many :votes, foreign_key: :voteable_id
+
+	
 end
