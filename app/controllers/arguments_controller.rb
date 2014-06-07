11d134ea27e71c2059d1494b318b548c2fea09f6
Anne Baer <albaer@gmail.com>
2014-06-07 18:56:03 -0500

    moved logic from view to controller

--- app/controllers/arguments_controller.rb
+++ app/controllers/arguments_controller.rb
@@ -11,6 +11,9 @@ class ArgumentsController < ApplicationController
   def show
   	@argument = Argument.find(params[:id])
     @position = @argument.is_supporting ? "supporting" : "opposing"
+    @conclusion = @argument.conclusion
+    @author = @argument.author
+    @premises = @argument.premises
   end
 
   def new
